2b7f11b01d4e52fc62e7c36469df9f590435e667
Andres Galaz <andres.galaz@gmail.com>
2020-02-27 11:12:46 -0300

    v0.2

--- bsh/compara.sh
+++ bsh/compara.sh
@@ -1,15 +1,19 @@
 #!/bin/bash -x
-JAVA_SRC=$HOME/workspace/javaUnitDesap/src
+RUTA=$(dirname $0)
+if [ "$RUTA" != "." ] ; then
+	cd "$RUTA"
+fi
+JAVA_SRC=$PWD/../../../../javaUnitDesap/src
 
-diff $JAVA_SRC/vyl/db/Venta.java			db/ventaClase.bsh 
-diff $JAVA_SRC/vyl/db/VentaArchivos.java	db/ventaArchivosClase.bsh 
-diff $JAVA_SRC/vyl/db/Comprador.java		db/compradorClase.bsh 
-diff $JAVA_SRC/vyl/db/Empresa.java			db/empresaClase.bsh 
-diff $JAVA_SRC/vyl/db/Leasing.java			db/leasingClase.bsh 
-diff $JAVA_SRC/vyl/db/Loteo.java			db/loteoClase.bsh 
+diff $JAVA_SRC/vyl/db/Venta.java			$PWD/db/ventaClase.bsh 
+diff $JAVA_SRC/vyl/db/VentaArchivos.java	$PWD/db/ventaArchivosClase.bsh 
+diff $JAVA_SRC/vyl/db/Comprador.java		$PWD/db/compradorClase.bsh 
+diff $JAVA_SRC/vyl/db/Empresa.java			$PWD/db/empresaClase.bsh 
+diff $JAVA_SRC/vyl/db/Leasing.java			$PWD/db/leasingClase.bsh 
+diff $JAVA_SRC/vyl/db/Loteo.java			$PWD/db/loteoClase.bsh 
 
-diff $JAVA_SRC/vyl/report/Formulario.java	report/formularioClase.bsh 
+diff $JAVA_SRC/vyl/report/Formulario.java	$PWD/report/formularioClase.bsh 
 
-diff $JAVA_SRC/util/JasperUtil.java			util/jasperUtilClase.bsh
-diff $JAVA_SRC/util/Rut.java				util/rutClase.bsh 
-diff $JAVA_SRC/prg/glz/wkf/RolFuncion.java	wkf/rolFuncionClase.bsh 
+diff $JAVA_SRC/util/JasperUtil.java			$PWD/util/jasperUtilClase.bsh
+diff $JAVA_SRC/util/Rut.java				$PWD/util/rutClase.bsh 
+diff $JAVA_SRC/prg/glz/wkf/RolFuncion.java	$PWD/wkf/rolFuncionClase.bsh 
