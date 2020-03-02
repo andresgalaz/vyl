29fed8cbb89827c5ee205978806ff9ae218174f7
Andres Galaz <andres.galaz@gmail.com>
2020-03-01 23:14:07 -0300

    docx

--- bsh/compara.sh
+++ bsh/compara.sh
@@ -13,7 +13,8 @@ diff $JAVA_SRC/vyl/db/Leasing.java			$PWD/db/leasingClase.bsh
 diff $JAVA_SRC/vyl/db/Loteo.java			$PWD/db/loteoClase.bsh 
 
 diff $JAVA_SRC/vyl/report/Formulario.java	$PWD/report/formularioClase.bsh 
+diff $JAVA_SRC/vyl/report/Escritura.java	$PWD/report/escrituraClase.bsh 
 
 diff $JAVA_SRC/util/JasperUtil.java			$PWD/util/jasperUtilClase.bsh
-diff $JAVA_SRC/util/Rut.java				$PWD/util/rutClase.bsh 
+# diff $JAVA_SRC/util/Rut.java				$PWD/util/rutClase.bsh 
 diff $JAVA_SRC/prg/glz/wkf/RolFuncion.java	$PWD/wkf/rolFuncionClase.bsh 
